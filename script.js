9b33c56f7fff638a6896738bb82246b71af12f2d
Niji Jose <nijijose378@gmail.com>
2021-11-09 11:43:22 +0530

    changes

---
+++ script.js
@@ -0,0 +1,25 @@
+let email = document.getElementById("mail");
+let msg = document.getElementById("error");
+
+function check(){
+    let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/
+    if(regexp.test(email.value)){
+        msg.innerHTML = "";
+        email.style.border = "4px solid green";
+        return true;
+    }
+    else{
+        msg.innerHTML = "Please enter a valid email id.";
+        msg.style.color = "red";
+        email.style.border = "4px solid red";
+        return false;
+    }
+
+    
+}
+    
+    
+
+
+
+
