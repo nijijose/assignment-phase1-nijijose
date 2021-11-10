2cc6198e6aa3eedb436d8640ad8ff8868d8b9cff
Niji Jose <nijijose378@gmail.com>
2021-11-10 16:48:39 +0530

    change

--- script.js
+++ script.js
@@ -46,37 +46,42 @@ var pass = document.getElementById("password");
 pass.addEventListener("keyup",function(){
       checkPassword(pass.value)
 })
-// function checkPassword(password){
-//     var strengthBar = document.getElementById("strength");
-//     var strength = 0;
-//     if(password.match(/^?=.*[a-z]+$/)){
-//       strength +=1;
-//     }
-//     if(password.match(/^?=.*[A-Z]+$/)){
-//       strength +=1;
-//     }
-//     if(password.match(/^?=.*[0-9]+$/)){
-//       strength += 1;
-//     }
-//     if(password.length >= 8){
-//       strength +=1;
-//     }
-//     switch(strength){
-//       case 0:
-//         strengthBar.value = 25;
-//         break;
-//       case 1:
-//         strengthBar.value = 50;
-//         break;
-//       case 2:
-//         strengthBar.value = 75;
-//         break;
-//       case 3:
-//         strengthBar.value = 100;
-//         break;  
-
-//     }
-// }
+function checkPassword(password){
+    var strengthBar = document.getElementById("strength");
+    var strength = 0;
+    if(password.match(/[a-z]+/)){
+      strength +=1;
+    }
+    if(password.match(/[A-Z]+/)){
+      strength +=1;
+    }
+    if(password.match(/[0-9]+/)){
+      strength += 1;
+    }
+    if(password.length >= 8){
+      strength +=1;
+    }
+    switch(strength){
+      case 0:
+        strengthBar.value = 0;
+        break;
+      case 1:
+        strengthBar.value = 25;
+        break;
+      case 2:
+        strengthBar.value = 50;
+        strengthBar.style.color = "yellow";
+        break;
+      case 3:
+        strengthBar.value = 75;
+        strengthBar.style.color = "yellow";
+        break; 
+      case 4:
+          strengthBar.value = 100;
+          strengthBar.style.color = "green";
+          break;   
+    }
+}
     
 
     
