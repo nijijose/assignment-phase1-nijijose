4c4ede5e6a6f45d4d39f9937b819339e66d8ecc2
Niji Jose <nijijose378@gmail.com>
2021-11-10 14:06:13 +0530

    change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let email = document.getElementById("mail");
 let msg = document.getElementById("error");
-let pass = document.getElementById("pwd");
+// let pass = document.getElementById("password");
 let name = document.getElementById("name");
 let phn = document.getElementById("num");
 let fedbck = document.getElementById("emsg");
@@ -33,7 +33,50 @@ function validate(){
         return false;
     }
 }
+function isGood(password) {
+    var password_strength = document.getElementById("password-text");
 
+    //TextBox left blank.
+    if (password.length == 0) {
+      password_strength.innerHTML = "";
+      return;
+    }
+
+    //Regular Expressions.
+    var regex = new Array();
+    regex.push("[A-Z]"); //Uppercase Alphabet.
+    regex.push("[a-z]"); //Lowercase Alphabet.
+    regex.push("[0-9]"); //Digit.
+    regex.push("[$@$!%*#?&]"); //Special Character.
+
+    var passed = 0;
+
+    //Validate for each Regular Expression.
+    for (var i = 0; i < regex.length; i++) {
+      if (new RegExp(regex[i]).test(password)) {
+        passed++;
+      }
+    }
+
+    //Display status.
+    var strength = "";
+    switch (passed) {
+      case 0:
+      case 1:
+      case 2:
+        strength = "<small class='progress-bar bg-danger' style='width: 40%'>Weak</small>";
+        break;
+      case 3:
+        strength = "<small class='progress-bar bg-warning' style='width: 60%'>Medium</small>";
+        break;
+      case 4:
+        strength = "<small class='progress-bar bg-success' style='width: 100%'>Strong</small>";
+        break;
+
+    }
+    password_strength.innerHTML = strength;
+
+  }
     
 
     
